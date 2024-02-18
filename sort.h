d088fb9613a70b8e86875a292b37aea704e11023
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-19 05:16:15 +0800

    modified sort.h header file

--- sort.h
+++ sort.h
@@ -32,9 +32,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-/*void sort_deck(deck_node_t **deck);*/
 void swap_func(int *left, int *right);
-
 
 
 #endif /*SORT_H*/
