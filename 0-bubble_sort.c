ad9dadc6df03905054cb790f731e3ffadf46a184
Obi Chukwu <kennetho.onu@gmail.com>
2024-02-19 10:36:00 +0800

    sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_func - swaps the position of two elements
+ * @left: the first selected element to be swapped
+ * @right: the second selected element to be swapped
+ */
+void swap_func(int *left, int *right)
+{
+        int temp;
+
+        temp = *left;
+        *left = *right;
+        *right = temp;
+}
+
+/**
  * bubble_sort - sorts an array of integers in ascending order
  *		using the bubble sort algorithm
  * @array: the array to be sorted
